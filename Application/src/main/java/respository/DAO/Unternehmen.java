676a917534b59b448db55dacf0948f84563a692c
jackderblaueaffe <36201515+jackderblaue@users.noreply.github.com>
2019-05-17 12:02:23 +0200

    Projektstruktur überarbeitet

---
+++ Application/src/main/java/respository/DAO/Unternehmen.java
@@ -0,0 +1,13 @@
+package respository.DAO;
+
+import java.awt.*;
+
+public class Unternehmen {
+    private long id;
+    private String name;
+    private Color farbe;
+    private boolean isAktiv;
+
+    //Liste Mitarbeiter
+    //SpielID
+}
