eae2bb1e43515148eb5b3a0234a43b4f3222bf0e
jcriecken <jc.riecken@gmail.com>
2019-06-05 16:44:01 +0200

    US 10: Perioden lassen sich anlegen und über die Oberfläche werden Kapitalmarktzins und Ordergebührt eingegeben.  Wechsel zum Detail Screen für das Eingeben der Kurse funktioniert noch nicht

--- src/main/java/de/dhbw/karlsruhe/controller/PeriodeAnlegenController.java
+++ src/main/java/de/dhbw/karlsruhe/controller/PeriodeAnlegenController.java
@@ -9,6 +9,8 @@ import de.dhbw.karlsruhe.model.jpa.Periode;
 import de.dhbw.karlsruhe.model.jpa.Wertpapier;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,6 +25,9 @@ public class PeriodeAnlegenController implements ControlledScreen {
 
     private KursRepository kursRepository = KursRepository.getInstanz();
 
+    @FXML
+    private TextField txtZins, txtOrder;
+
     @Override
     public void setScreenParent(ScreenController screenPage) {
         this.screenController = screenPage;
@@ -30,13 +35,27 @@ public class PeriodeAnlegenController implements ControlledScreen {
 
     @FXML
     private void doPeriodeAnlegen(ActionEvent event) {
-        //ToDo Carlos: TF einlesen usw.
-        Periode periode = new Periode(AktuelleSpieldaten.getSpiel(), 0, 0);
+        Double ordergebuehr, kapitalzins;
+        try {
+            ordergebuehr = Double.valueOf(txtOrder.getText());
+            kapitalzins = Double.valueOf(txtZins.getText());
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Periode anlegen");
+            alert.setContentText("Bitte geben Sie Zahlen ein.");
+            alert.showAndWait();
+            return;
+        }
+        Periode periode = new Periode(AktuelleSpieldaten.getSpiel(),  ordergebuehr, kapitalzins);
         periodenRepository.save(periode);
 
         List<Wertpapier> wertpapiere = wertpapierRepository.findAll();
         List<Kurs> kurse = wertpapiere.stream().map(wertpapier -> new Kurs(periode, wertpapier)).collect(Collectors.toList());
         kursRepository.save(kurse);
+        //Klappt nicht richtig... Im screen Perioden detail sollten die Kurswerte der Periode eingetragen werden können. Der screen lässt sich jedoch nicht wechseln
+        screenController.loadScreen(ScreensFramework.SCREEN_PERIODEN_DETAIL, "scene_perioden_detail.fxml");
+        screenController.setScreen(ScreensFramework.SCREEN_PERIODEN_DETAIL);
     }
 
 }
