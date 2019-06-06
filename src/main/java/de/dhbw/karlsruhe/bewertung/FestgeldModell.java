324c13791bfa6b8e4857b518f7298de30cb951d2
KarelZe <mail@markusbilz.com>
2019-06-07 00:24:16 +0200

    Aufnahme Bepreisungsmodell Festgeld [US 10] - Implementierung Bewertungsmodell Festgeld

--- src/main/java/de/dhbw/karlsruhe/bewertung/FestgeldModell.java
+++ src/main/java/de/dhbw/karlsruhe/bewertung/FestgeldModell.java
@@ -5,8 +5,18 @@ import de.dhbw.karlsruhe.model.jpa.Periode;
 import de.dhbw.karlsruhe.model.jpa.Wertpapier;
 
 public class FestgeldModell implements Bewertungsmodell {
+    /**
+     * Implementierung der Bewertungsmodells für Festgelder. Festgelder sind im Datenmodell als Wertpapier implementiert.
+     * Da Festgelder keinen Kurswertrisiken unterliegen, erfolgt eine Bewertung zu 100.00 %.
+     *
+     * @param periode    Periode, für die eine Bewertung erfolgen soll.
+     * @param wertpapier Wertpapier, das zu bewerten ist.
+     * @return Kurs des Festgelds
+     */
     @Override
     public Kurs bewerte(Periode periode, Wertpapier wertpapier) {
-        return null;
+        Kurs kurs = new Kurs(periode, wertpapier);
+        kurs.setKurs(100.00d);
+        return kurs;
     }
 }
