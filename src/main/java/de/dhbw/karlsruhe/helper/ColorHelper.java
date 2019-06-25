83129f76b74488ceadd86415d5256f797f1733d8
KarelZe <mail@markusbilz.com>
2019-06-25 06:41:45 +0200

    Dokumentation [US Div] - ColorHelper (Korrektur) - NumberHelper (Neu)

--- src/main/java/de/dhbw/karlsruhe/helper/ColorHelper.java
+++ src/main/java/de/dhbw/karlsruhe/helper/ColorHelper.java
@@ -3,7 +3,7 @@ package de.dhbw.karlsruhe.helper;
 import javafx.scene.paint.Color;
 
 /**
- * Diese Klasse konvertiert Color-Objekte in String-Objekte und umgekehrt.
+ * Diese Klasse stellt Bequemlichkeitsmethoden zur Konvertierung von Color-Objekte in String-Objekte und umgekehrt bereit.
  * Strings werden für die Speicherung in der Datenbank o. Ä. benötigt.
  *
  * @author Markus Bilz
@@ -12,7 +12,7 @@ public class ColorHelper {
 
     /**
      * Diese Methode konvertiert einen String in ein Color-Objekt.
-     * @param farbe Farbe zur Konveritierung
+     * @param farbe Farbe zur Konvertierung
      * @return Color Objekt der Farbe, sofern keine Konvertierung möglich ist, Color Objekt der Farbe Schwarz
      */
     public static Color string2Color(String farbe) {
@@ -32,7 +32,7 @@ public class ColorHelper {
     }
 
     /**
-     * Konvertiert ein Color-Objekt in einen String. Die String-Repräsentation hat folgenden Aufbau @code{0x000000ff}.
+     * Konvertiert ein Color-Objekt in einen String. Die String-Repräsentation hat folgenden Aufbau {@code 0x000000ff}.
      *
      * @param farbe Farbe zur Konvertierung
      * @return Farbe als String
--- src/main/java/de/dhbw/karlsruhe/helper/NumberHelper.java
+++ src/main/java/de/dhbw/karlsruhe/helper/NumberHelper.java
@@ -1,11 +1,24 @@
 package de.dhbw.karlsruhe.helper;
 
+/**
+ * Diese Klasse stellt Bequemlichkeitsmethoden zur Konvertierung von Strings in Zahlen bereit.
+ *
+ * @author Markus Bilz
+ */
+
 public class NumberHelper {
-    public static double parseDouble(String string, double defaultValue) {
+    /**
+     * Methode zur Konvertierung eines String in einen double
+     *
+     * @param zahl         Zahl zur Konvertierung
+     * @param standardwert Standardwert im Fehlerfall
+     * @return double der Zahl, im Fehlerfall den Standardwert
+     */
+    public static double parseDouble(String zahl, double standardwert) {
         try {
-            return Double.parseDouble(string);
+            return Double.parseDouble(zahl);
         } catch (NumberFormatException e) {
-            return defaultValue;
+            return standardwert;
         }
     }
 }
