d2b551e31a55dbda663860c6504459ca35592147
KarelZe <mail@markusbilz.com>
2019-06-25 22:01:19 +0200

    Kommentierung Portfolioposition [US Div]

--- src/main/java/de/dhbw/karlsruhe/model/fassade/Portfolioposition.java
+++ src/main/java/de/dhbw/karlsruhe/model/fassade/Portfolioposition.java
@@ -2,11 +2,22 @@ package de.dhbw.karlsruhe.model.fassade;
 
 import de.dhbw.karlsruhe.model.jpa.Wertpapier;
 
+/**
+ * Wrapper um Wertapapierklasse zum vereinfachten Handling von Wertpapier-Objekten für den Handel von Wertpapieren.
+ * Eine Verwendung ist insbesondere in Verbindung mit {@link PortfolioFassade} sinnvoll.
+ *
+ * @author Markus Bilz
+ */
 public class Portfolioposition {
 
     Wertpapier wertpapier;
     double bezugsgroesse;
 
+    /**
+     * Kontruktor für die Erzeugung einer Portfolioposition.
+     * @param wertpapier Wertpapier der Portfolioposition
+     * @param bezugsgroesse Stückzahl oder Nennwert der Wertpapierposition
+     */
     public Portfolioposition(Wertpapier wertpapier, double bezugsgroesse) {
         this.wertpapier = wertpapier;
         this.bezugsgroesse = bezugsgroesse;
