cb253d4e3c657c757064bf2f6b7523bb99da9499
KarelZe <mail@markusbilz.com>
2019-06-25 06:18:20 +0200

    Dokumentation ColorHelper [US Diverse]

--- src/main/java/de/dhbw/karlsruhe/helper/ColorHelper.java
+++ src/main/java/de/dhbw/karlsruhe/helper/ColorHelper.java
@@ -2,8 +2,19 @@ package de.dhbw.karlsruhe.helper;
 
 import javafx.scene.paint.Color;
 
+/**
+ * Diese Klasse konvertiert Color-Objekte in String-Objekte und umgekehrt.
+ * Strings werden für die Speicherung in der Datenbank o. Ä. benötigt.
+ *
+ * @author Markus Bilz
+ */
 public class ColorHelper {
 
+    /**
+     * Diese Methode konvertiert einen String in ein Color-Objekt.
+     * @param farbe Farbe zur Konveritierung
+     * @return Color Objekt der Farbe, sofern keine Konvertierung möglich ist, Color Objekt der Farbe Schwarz
+     */
     public static Color string2Color(String farbe) {
         System.out.println(farbe);
         String farbeGekuerzt = farbe.substring(2);
@@ -20,8 +31,14 @@ public class ColorHelper {
         return new Color(r / 255.0, g / 255.0, b / 255.0, o / 255.0);
     }
 
-    public static String color2String(Color color) {
-        return color.toString();
+    /**
+     * Konvertiert ein Color-Objekt in einen String. Die String-Repräsentation hat folgenden Aufbau @code{0x000000ff}.
+     *
+     * @param farbe Farbe zur Konvertierung
+     * @return Farbe als String
+     */
+    public static String color2String(Color farbe) {
+        return farbe.toString();
 
     }
 }
