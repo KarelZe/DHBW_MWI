c42134de68b5cbf8a1345d3a3c837d7e450554bd
KarelZe <mail@markusbilz.com>
2019-06-26 06:49:06 +0200

    Kommentierung Aktuelle Spieladaten [US Div]

--- src/main/java/de/dhbw/karlsruhe/model/AktuelleSpieldaten.java
+++ src/main/java/de/dhbw/karlsruhe/model/AktuelleSpieldaten.java
@@ -3,6 +3,13 @@ package de.dhbw.karlsruhe.model;
 import de.dhbw.karlsruhe.model.jpa.Spiel;
 import de.dhbw.karlsruhe.model.jpa.Teilnehmer;
 
+/**
+ * Diese Klasse stellt Methoden zur Abfrage des aktuellen Spiels und des eingeloggten Teilnehmers zur Verfügung.
+ * Sie repräsentiert eine Art Session, welche die aktuelle Konfiguration des Spiels enthält. Die Klasse
+ * muss muss für den Zugriff von Rollen- oder Spielbasierten Zugriffen verwendet werden.
+ *
+ * @author Christian Fix
+ */
 public class AktuelleSpieldaten {
     private static Teilnehmer teilnehmer;
     private static Spiel spiel;
